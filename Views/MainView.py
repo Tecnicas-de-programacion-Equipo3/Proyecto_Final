4f52f90aff6ac0967f0f81068c92acd3498e2826
gustavo santos <gustavo.santos.nunez@gmail.com>
2017-11-14 23:44:58 -0600

    Inicio de interfaz

--- Views/MainView.py
+++ Views/MainView.py
@@ -1,10 +1,19 @@
-from tkinter import Tk
+from tkinter import Tk , PhotoImage
+from Views.Labels import BackGround
 
 class MainView(Tk):
+    class Images:
+        house = "assets/BackGhouse.png"
+    class Positions:
+        x_house = 100
+        y_house = 5
+
     class Constants:
         title = "Smart House"
-        heigth = 600
-        width = 800
+        heigth = 700
+        width = 1000
+        widthB =600
+        bg = "#eee8dc"
 
         @classmethod
         def size(cls):
@@ -14,3 +23,10 @@ class MainView(Tk):
         super().__init__()
         self.title(self.Constants.title)
         self.geometry(self.Constants.size())
+        self.configure(bg = self.Constants.bg)
+        self.__interfaz_configure()
+
+    def __interfaz_configure(self):
+
+        self.__the_house = PhotoImage(file=self.Images.house)
+        self.__backG = BackGround(self, self.Positions.x_house, self.Positions.y_house, image=self.__the_house, width=self.Constants.widthB)
