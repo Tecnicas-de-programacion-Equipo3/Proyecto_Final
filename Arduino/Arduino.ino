08ae8f1dbefcfd5b3eb24492b4e4bbbc662eb775
sergilgg <sergil51@hotmail.com>
2017-11-25 20:08:55 -0600

    Correcccion nombre de variables

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -7,12 +7,12 @@ int led_1 = 13;
 int led_2 = 12;
 int led_3 = 11;
 int led_4 = 10;
-int vent = 9;
-int state1 = 0;
-int state2 = 0;
-int state3 = 0;
-int state4 = 0;
-int state5 = 0;
+int fan = 9;
+int state_led_1 = 0;
+int state_led_2 = 0;
+int state_led_3 = 0;
+int state_led_4 = 0;
+int state_fan = 0;
 int Temp = 2;
 OneWire oneWire(Temp); 
 DallasTemperature sensors(&oneWire);
@@ -30,8 +30,8 @@ void setup() {
    pinMode(led_4, OUTPUT);
    digitalWrite(led_4, LOW);
 
-   pinMode(vent, OUTPUT);
-   digitalWrite(vent, LOW);
+   pinMode(fan, OUTPUT);
+   digitalWrite(fan, LOW);
    sensors.begin(); 
    
 
@@ -40,31 +40,31 @@ void setup() {
 void loop(void) { 
   
   sensors.requestTemperatures();
-  Serial.print("\n"); 
   Serial.print(sensors.getTempCByIndex(0));
+  Serial.print("\n"); 
   }
 
 void serialEvent() {
     char inChar = (char)Serial.read();
-    if (inChar == '1') state1 = HIGH;
-    if (inChar == 'a') state1 = LOW;
-    digitalWrite(led_1, state1);    
+    if (inChar == '1') state_led_1 = HIGH;
+    if (inChar == 'a') state_led_1 = LOW;
+    digitalWrite(led_1, state_led_1);    
      
-    if (inChar == '2') state2 = HIGH;
-    if (inChar == 'b') state2 = LOW;
-    digitalWrite(led_2, state2); 
+    if (inChar == '2') state_led_2 = HIGH;
+    if (inChar == 'b') state_led_2 = LOW;
+    digitalWrite(led_2, state_led_2); 
     
-    if (inChar == '3') state3 = HIGH;
-    if (inChar == 'c') state3 = LOW;
-    digitalWrite(led_3, state3); 
+    if (inChar == '3') state_led_3 = HIGH;
+    if (inChar == 'c') state_led_3 = LOW;
+    digitalWrite(led_3, state_led_3); 
     
-    if (inChar == '4') state4 = HIGH;
-    if (inChar == 'd') state4 = LOW;
-    digitalWrite(led_4, state4); 
+    if (inChar == '4') state_led_4 = HIGH;
+    if (inChar == 'd') state_led_4 = LOW;
+    digitalWrite(led_4, state_led_4); 
 
-    if (inChar == '5') state5 = HIGH;
-    if (inChar == 'e') state5 = LOW;
-    digitalWrite(vent, state5); 
+    if (inChar == '5') state_fan = HIGH;
+    if (inChar == 'e') state_fan = LOW;
+    digitalWrite(fan, state_fan); 
 
     
 }
