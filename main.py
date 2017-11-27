c58b57567e7098ec2fb0c8faaab43b3cfb2f7eae
gustavo santos <gustavo.santos.nunez@gmail.com>
2017-11-26 22:25:59 -0600

    Integrate to the master

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ class MainApp():
     def run(self):
         self.__master.mainloop()
 
-    def __toggle_did_change(self, state ):
-        pass
+    def __toggle_did_change(self, state):
+        value = str(1 if state else 0).encode('ascii')
 
 if __name__ == "__main__":
     app = MainApp()
