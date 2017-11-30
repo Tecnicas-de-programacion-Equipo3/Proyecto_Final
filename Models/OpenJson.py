31f58427b27191dff1df37c1905a0226b81d9231
LauraReynosa <laliz0197@gmail.com>
2017-11-30 13:49:15 -0600

    Adding open json

---
+++ Models/OpenJson.py
@@ -0,0 +1,11 @@
+import json
+
+class OpenJson:
+    @staticmethod
+    def oppening(file):
+        try:
+            with open(file + '.json', r) as in_file:
+                json_file = json.load(in_file)
+        except Exception:
+            return None
+        return json_file
