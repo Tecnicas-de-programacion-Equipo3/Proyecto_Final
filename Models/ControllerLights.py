c677fe0d7a82960f877cd78b1324ff91a6456bf5
LauraReynosa <33081297+LauraReynosa@users.noreply.github.com>
2017-11-30 19:20:55 -0600

    Revert "ControllerGarage created to conect the toggle button with arduino"

--- Models/ControllerLights.py
+++ Models/ControllerLights.py
@@ -6,6 +6,7 @@ class ControllerLights():
         Livingroom = "Livingroom"
         Diningroom = "Diningroom"
 
+
     class Constants:
         Bedroom1 = "1"
         Bedroom2 = "2"
@@ -14,6 +15,8 @@ class ControllerLights():
         on = "1"
         off = "0"
 
+
+
     def __init__(self,state, room, lights_handler):
         self.__lights_handler = lights_handler
         self.__state = state
@@ -21,6 +24,9 @@ class ControllerLights():
         self.__turn_on = True
         self.__turn_off = False
         self.__control_lights()
+
+
+
 
     def __state_room(self):
         if self.__state == self.__turn_on:
@@ -40,6 +46,8 @@ class ControllerLights():
              return self.Constants.Diningroom
 
     def __control_lights(self):
+
+
         state_room = self.__state_room()
         room = self.__room()
         self.__lights_handler(room+state_room)
